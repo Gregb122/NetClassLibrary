8a57de3cf4e7fca68c2279c44efcb747485e0c7f
Grzegorz Bielecki <288388@uwr.edu.pl>
2019-05-18 02:52:44 +0200

    Add project files.

---
+++ ComplexNumbers/Complex.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ComplexNumbers
+{
+    class Complex : IFormattable
+    {
+        private System.Numerics.Complex ComplexNum { get; set; }
+
+        public Complex(System.Numerics.Complex complex)
+        {
+            ComplexNum = complex;
+        }
+
+        public Complex(Double a , Double b)
+        {
+            ComplexNum = new System.Numerics.Complex(a, b);
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.ComplexNum + b.ComplexNum);
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.ComplexNum - b.ComplexNum);
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            return new Complex(a.ComplexNum / b.ComplexNum);
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.ComplexNum * b.ComplexNum);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format)) format = "d";
+            if (formatProvider == null) formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "D":
+                    return String.Format("{0} + {1}i", ComplexNum.Real, ComplexNum.Imaginary);
+                case "W":
+                    return String.Format("[{0},{1}]", ComplexNum.Real, ComplexNum.Imaginary);
+                default:
+                    throw new FormatException(String.Format("The {0} format string is not supported.", format));
+            }
+
+        }
+    }
+}
---
+++ ComplexNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex z = new Complex(4, 3);
+            Console.WriteLine(String.Format("{0}", z));
+            Console.WriteLine(String.Format("{0:d}", z));
+            Console.WriteLine(String.Format("{0:w}", z));
+        }
+    }
+}
