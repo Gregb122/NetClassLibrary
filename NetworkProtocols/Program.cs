f6dcd261dad48b9f032a478924794da5d81cdf46
Grzegorz Bielecki <288388@uwr.edu.pl>
2019-05-21 02:16:33 +0200

    WebProtocols

---
+++ NetworkProtocols/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProtocols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FtpWebRequest request = (FtpWebRequest)WebRequest.Create("jakiś serwer ftp");
+            //request.Method = WebRequestMethods.Ftp.DeleteFile;
+            //FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            //response.Close();
+
+            //--------------------------------------------------------------------------------------------------------------------
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the 
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            Stream data = client.OpenRead("https://www3.gogoanime.io/");
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            data.Close();
+            reader.Close();
+        }
+
+        static async Task Main()
+        {
+            // Create a New HttpClient object and dispose it when done, so the app doesn't leak resources
+            using (HttpClient client = new HttpClient())
+            {
+                // Call asynchronous network methods in a try/catch block to handle exceptions
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+        }
+
+    }
+}
